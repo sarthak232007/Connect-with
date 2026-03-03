caf47d5848b61b564e5a260e51d79b4bed92fc82
sarthak <sarthakdixit547@gmail.com>
2026-03-03 22:07:45 +0530

    app.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    fetch(`${API_URL}/health`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((err) => {
+        console.error("Error connecting to backend:", err);
+        setMessage("Backend connection failed");
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Frontend Connected ✅</h1>
+      <h2>{message}</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

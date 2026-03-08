77c2c5344d0926e869dcefc6867f664758477024
sarthak <sarthakdixit547@gmail.com>
2026-03-08 15:03:51 +0530

    frontend-tools

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,49 @@
-import { useEffect, useState } from "react";
-import "./App.css";
 import { SignInButton } from "@clerk/clerk-react";
 import { SignOutButton } from "@clerk/clerk-react";
 import { SignedOut } from "@clerk/clerk-react";
 import { SignedIn } from "@clerk/clerk-react";
 import { UserButton } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ProblemsPage from "./pages/ProblemsPage.jsx"; 
+import { Toaster } from "react-hot-toast"; 
 function App() {
-  
+   const {isSignedIn}=useUser();
   return (
-    
     <>
-  <h1>Welcome to the app</h1>
-
-  <SignedOut>
-    <SignInButton mode="modal">
-      <button>Log in</button>
-    </SignInButton>
-  </SignedOut>
-
-  <SignedIn>
-    <SignOutButton />
-    <UserButton />
-  </SignedIn>
-</>
-
+    <Routes>
+     <Route path="/" element={<HomePage />} />
+     <Route path="/about" element={<AboutPage />} />
+     <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <HomePage />} />
+    </Routes>
+    
+    <Toaster/>
+    </>
   );
+    
+//   <h1>Welcome to the app</h1>
+
+// <button className="btn btn-primary">Click me </button>
+
+
+//   <SignedOut>
+//     <SignInButton mode="modal">
+//       <button>Log in</button>
+//     </SignInButton>
+//   </SignedOut>
+
+//   <SignedIn>
+//     <SignOutButton />
+//     </SignedIn>
+    
+//      <UserButton />
+    //  </routes>
+
+ 
+
+
+   
 }
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react';
+
+import { BrowserRouter } from 'react-router'; 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 // publishable key 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -10,10 +14,17 @@ if (!PUBLISHABLE_KEY) {
   console.error("Error: VITE_CLERK_PUBLISHABLE_KEY is not defined in the environment variables.");
 } 
 
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
+    <QueryClientProvider client = {queryClient}>
      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
+    </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
